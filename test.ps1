d8bfcd78a0347899c59a3eff4c36c2061c1ce673
Alex Lopez <info@ithinkvirtual.com>
2020-02-28 15:58:09 -0500

    Initial commit

---
+++ test.ps1
@@ -0,0 +1,37 @@
+
+"Running tests"
+$ErrorActionPreference = "Stop"
+
+
+if ($env:APPVEYOR_BUILD_VERSION) {
+  # run in CI
+  $version = $env:APPVEYOR_BUILD_VERSION -replace('\.[^.\\/]+$')
+} else {
+  # run manually
+  [xml]$spec = Get-Content vmware-converter.nuspec
+  $version = $spec.package.metadata.version
+}
+
+"TEST: Version $version in vmware-converter.nuspec file should match"
+[xml]$spec = Get-Content vmware-converter.nuspec
+if ($spec.package.metadata.version.CompareTo($version)) {
+  Write-Error "FAIL: Wrong version in nuspec file!"
+}
+
+"TEST: Package should contain install/uninstall scripts"
+Add-Type -assembly "system.io.compression.filesystem"
+$zip = [IO.Compression.ZipFile]::OpenRead("$pwd\vmware-converter.$version.nupkg")
+# Write-Host $zip.Entries.FullName
+Write-Host $zip.Entries.Count
+if ($zip.Entries.Count -ne 6) {
+  Write-Error "FAIL: Wrong count in nupkg!"
+}
+$zip.Dispose()
+
+"TEST: Installation of package should work"
+. choco install -y vmware-converter -source . -version $version
+
+"TEST: Uninstallation of package should work"
+. choco uninstall -y vmware-converter -source .
+
+"TEST: Finished"
